a24530e005104054fc67625f214451b75d8d74d9
HarmandI <isabelle44.74@gmail.com>
2022-01-17 18:20:39 +0100

    récupère les ID  des produits dans la page détail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardPlanteComponent } from './components/card-plante/card-plante.component';
 import { PageAccueilComponent } from './pages/page-accueil/page-accueil.component';
 import { PageDetailsComponent } from './pages/page-details/page-details.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
+
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: PageAccueilComponent },
-  { path: 'details', component: PageDetailsComponent },
+  { path: 'details/:productId', component: CardPlanteComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
